fac45f2829ca776f19070830227eecf05ae76aa1
veronica365 <naruth10@gmail.com>
2023-03-14 13:57:36 +0300

    Add functionality for the header

--- src/modules/paint-app.js
+++ src/modules/paint-app.js
@@ -1,2 +1,15 @@
-const initialize = () => 'Hello world';
+const addHeader = () => `
+        <nav>
+            <div class="logo">TV Shows</div>
+            <ul>
+                <li>Home</li>
+                <li>Discover</li>
+                <li>Trending</li>
+            </ul>
+        </nav>`;
+
+const initialize = () => {
+  document.querySelector('header').innerHTML = addHeader();
+};
+
 export default initialize;
